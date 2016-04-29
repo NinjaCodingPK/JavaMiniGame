2fbc9ec1fcb705f3c54db8e64eca12cfc4aef3f1
wookie <kononenkoa995@ukr.net>
2016-04-29 17:32:08 +0300

    global constants class

---
+++ src/model/GlobalConstants.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Created by wookie on 4/29/16.
+ */
+public abstract class GlobalConstants {
+    public static final int START_LOWER_BORDER = 0;
+    public static final int START_UPPER_BORDER = 100;
+}
